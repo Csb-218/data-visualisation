6be9196395cf9199cdd8abc8317061bfffa8d607
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-12-14 19:55:48 +0530

    fix : removed redux state initiation from url using useSearchParams

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,51 +1,23 @@
 'use client'
-import { useRef,useEffect,Suspense } from 'react'
+import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore, AppStore } from '@/lib/store'
-import { useSearchParams } from 'next/navigation';
-import { setAge , setGender } from '@/lib/store/barChartSlice'
+import { makeStore,AppStore } from '@/lib/store'
 
-function StoreProviderComponent({children}: {children: React.ReactNode}) {
-
-  const storeRef = useRef<AppStore | null>(null)
-  const searchParams = useSearchParams();
-
-  const paramGender = searchParams.get('gender');
-  const paramAge = searchParams.get('age');
-
-
-
-  useEffect(()=>{
-
-    if (!storeRef.current) {
-      // Create the store instance the first time this renders
-      storeRef.current = makeStore()
+export default function StoreProvider({ children }: {
+  children: React.ReactNode
+}) {
+  const storeRef = useRef<AppStore|null>(null)
   
-      if(paramAge) storeRef.current.dispatch(setAge(paramAge))
-      if(paramGender) storeRef.current.dispatch(setGender(paramGender))
-  
-    }
-
-
-  },[paramAge,paramGender,searchParams])
-
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
     storeRef.current = makeStore()
-
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>
-}
 
-export default function StoreProvider({children}: {children: React.ReactNode}){
-
-  return(
-    <Suspense fallback={<>Loading ...</>}>
-      <StoreProviderComponent >
-      {children}
-      </StoreProviderComponent>
-    </Suspense>
-
-  )
+  return (
+    <Provider store={storeRef.current}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        {children}
+      {/* </PersistGate> */}
+    </Provider>
+  );
 }
