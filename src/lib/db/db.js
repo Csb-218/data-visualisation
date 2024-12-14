d3e72de82d6662f4eb0ab0c3625a1e26cf014636
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-12-14 23:05:38 +0530

    Update db.js

--- src/lib/db/db.js
+++ src/lib/db/db.js
@@ -6,7 +6,7 @@ import { open } from 'sqlite';
 async function openDb(){
      
     const connection = await open({
-      filename: process.env.NEXT_PUBLIC_DATABASE_URL,
+      filename: `${process.env.NEXT_PUBLIC_DATABASE_URL}`,
       driver: sqlite3.Database,
      });
 
