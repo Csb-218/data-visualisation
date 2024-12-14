cad510ea35ffe8310dbd929c3d58385aa964c441
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-12-15 02:25:26 +0530

    feat : added share button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import  Line_chart  from "@/components/charts/line_chart";
 import UserCard from "@/components/miscellaneous/UserCard";
 import CookieModal from '@/components/miscellaneous/CookieModal'
 import {Button} from '@/components/ui/button'
+import { Share2 } from "lucide-react"
 import  {deleteCookie} from 'cookies-next'
 import { googleLogout } from "@react-oauth/google";
 import { useRouter } from "next/navigation";
@@ -15,6 +16,27 @@ export default function Home() {
 
 
    const router = useRouter()
+
+   const handleShareClick = () => {
+    const currentUrl = window.location.href;
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(currentUrl)
+        .then(() => {
+          alert('URL copied to clipboard!');
+        })
+        .catch(err => {
+          console.error('Failed to copy URL: ', err);
+        });
+    } else {
+      const textArea = document.createElement('textarea');
+      textArea.value = currentUrl;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+      alert('URL copied to clipboard!');
+    }
+  };
 
    
   // if (isLoading) return <div>Loading...</div>;
@@ -41,6 +63,15 @@ export default function Home() {
 
           <HorBar />
           <Line_chart />
+
+          <Button 
+          className='mt-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white hover:text-white ' 
+          variant={"outline"} 
+          onClick={handleShareClick}>
+              <Share2/> Share your chart
+            </Button> 
+
+          
 
 
         </main>
